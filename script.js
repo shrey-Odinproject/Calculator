ccda491387fd7d711eec61e63572922496ef003c
shrey-Odinproject <sonishreyf1@gmail.com>
2021-11-05 17:02:24 +0530

    created barebones calculator outline with css grid , calc is static without js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function add(a, b) { return a + b }
 function sub(a, b) { return a - b }
 function mul(a, b) { return a * b }
-function divide(a, b) { return Math.round((a/b)*100)/100 }
+function divide(a, b) { return Math.round((a / b) * 100) / 100 }
 
 function operate(a, op, b) {
     if (op === '+') { return add(a, b) }
@@ -10,4 +10,37 @@ function operate(a, op, b) {
     else if (op === '/') { return divide(a, b) }
     else { return 'INVALID' }
 }
+
+let buttonsOnPad = 15
+let buttonPadRow=5
+let buttonPadCol=3
+
+const body = document.querySelector('body')
+const calc = body.querySelector('.calculator')
+const calcScreen = document.createElement('div')
+
+calcScreen.classList.add('calcScreen')
+calc.appendChild(calcScreen)
+
+const calcPad = document.createElement('div')
+calcPad.setAttribute('style',
+    `
+    grid-template-rows:${'auto '.repeat(buttonPadRow)};
+    grid-template-columns:${'auto '.repeat(buttonPadCol)};
+    `)
+calcPad.classList.add('calcPad')
+calc.appendChild(calcPad)
+
+calcScreen.textContent = '1+3'
+
+function makeButtons() {
+    for (let i = 0; i < buttonsOnPad; i++) {
+        const calcPadButton = document.createElement('button')
+        calcPadButton.classList.add('calcPadButton')
+        calcPadButton.textContent='0'
+        calcPad.appendChild(calcPadButton)
+    }
+}
+
+makeButtons()
 
